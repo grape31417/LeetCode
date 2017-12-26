40f91089f098897e1fca63f5e71c4e4ad5a5d76f
wally <1099105319@kuas.edu.tw>
2017-12-26 16:25:08 +0800

    無條件進位

---
+++ 無條件進位/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float a;
+        int b;
+
+        a= (float)- 3.7;
+        b= (int) a;
+
+        if(a-b>0)
+        System.out.printf(String.valueOf(b+1));
+        else
+        System.out.printf(String.valueOf(b));
+
+    }
+}
